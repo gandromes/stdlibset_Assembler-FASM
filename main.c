f655e5410b517146feef6dbc6ac421444ed8a299
QuickYoung <moto.pers2007@gmail.com>
2023-03-27 15:49:09 +0400

    refactoring(main.c): deleted unnecessary examples

--- main.c
+++ main.c
@@ -1,77 +1,27 @@
 #include "c_lib/fmt.h"
-#include "c_lib/sys.h"
-#include "c_lib/mth.h"
-#include "c_lib/srt.h"
 #include "c_lib/str.h"
+#include "c_lib/sys.h"
 
 typedef unsigned long long int _uint64_t;
 typedef unsigned char _uint8_t;
 
-#define SIZE 120
-#define LENGTH(arr) sizeof(arr) / sizeof(arr[0])
+#define SIZE 100
 
 int main(void)
 {
-  int gcdNUM = c_gcd(1240, 790);
-  c_print_number(gcdNUM);
-  // int n = c_input_number();
-  // c_print_number(n);
-  // c_print_empty_line();
-  // c_print_bin(n);
-  // c_print_empty_line();
-  // c_print_oct(n);
-  // c_print_empty_line();
-  // c_print_hex(n);
-  // c_print_empty_line();
+  _uint8_t* data = "#include <stdio.h>\n\nint main(void)\n{\n\tprintf(\"Hello world!\\n\");\n}";
+  _uint64_t data_size = c_length_string(data);
+  _uint8_t* filename = "xxx.c";
 
-  // _uint64_t time_now = c_time_now();
-  // c_srand(time_now);
-  // c_print_number(c_rand());
-  // c_print_empty_line();
-  //
-  // c_print_f("[%d]\n", c_gcd(3712, 1234));
-  // c_print_f("[%d]\n", c_factorial(5));
+  _uint64_t descriptor = c_fcreate(filename, 0666);
+  c_fwrite(descriptor, data, data_size);
+  c_fclose(descriptor);
 
-  // c_print_string("Take number to calc Fibonacci: ");
-  // _uint64_t n = c_input_number();
-  // c_print_f("[%d]\n", c_fibonacci(n));
+  static _uint8_t buffer[SIZE];
+  _uint64_t descript = c_fopen(filename, 0);
+  c_fread(descript, buffer, SIZE);
+  c_print_f("%s\n", buffer);
 
-  // _uint8_t arrayNumber[7] = {1, 3, 0, 92, 123, 22, 11};
-  // c_print_bytes(arrayNumber, LENGTH(arrayNumber));
-  // c_bubble_sort(arrayNumber, LENGTH(arrayNumber));
-  // c_print_empty_line();
-  // c_print_bytes(arrayNumber, LENGTH(arrayNumber));
-  // c_print_empty_line();
-
-  // char* str = "Hello, world!";
-  // c_print_number(c_length_string(str));
-  // c_print_empty_line();
-
-  // char* strNUM = "567";
-  // int NUM = c_string_to_number(strNUM);
-  // c_print_f("%d + %d = %d\n", NUM, NUM, NUM + NUM);
-
-  // int num = 567;
-  // static _uint8_t STR[4];
-  // c_number_to_string(num, STR, 4);
-  // c_print_f("string -> %s\n", STR);
-
-  // char* past = "#include <stdio.h>\n\nint main(void)\n{\n\tprintf(\"Hello world!\\n\");\n}";
-  // _uint64_t descriptor = c_fcreate("xxx.c", 0666);
-  // c_fwrite(descriptor, past, c_length_string(past));
-  // c_fclose(descriptor);
-
-  // char* hi = "Hello, world";
-  // _uint64_t descriptor = c_fopen("xxx.c", 1);
-  // c_fwrite(descriptor, hi, c_length_string(hi));
-  // c_fclose(descriptor);
-
-  // static _uint8_t buffer[SIZE];
-  // _uint64_t descriptor = c_fopen("xxx.c", 0);
-  // c_fread(descriptor, buffer, SIZE);
-  // c_print_f("%s\n", buffer);
-
-  // c_fdelete("xxx.c");
-
-  return 0;
+  c_fdelete(filename);
+  c_exit();
 }
